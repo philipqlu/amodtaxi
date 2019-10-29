2a1eb9ca4acd7c87643558d61de503ddca91a702
clruch <claudio.ruch@gmail.com>
2019-10-29 06:09:26 +0100

    works pretty well...

--- src/main/java/ch/ethz/idsc/amodtaxi/scenario/zurichtaxi/ZurichOSMLocationFinder.java
+++ src/main/java/ch/ethz/idsc/amodtaxi/scenario/zurichtaxi/ZurichOSMLocationFinder.java
@@ -1,5 +1,6 @@
 package ch.ethz.idsc.amodtaxi.scenario.zurichtaxi;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -20,23 +21,47 @@ public class ZurichOSMLocationFinder {
         if (Objects.isNull(longLat)) {
 
             List<String> elements = TaxiAddress.prepare(address);
-            System.out.println("Elements: ");
-            System.out.println(elements);
-            System.out.println("---");
 
-            String https_url = NomatimURL.build(elements);
-            String returnJSONString = NominatimHelper.queryInterface(https_url);
-            JSONObject queryJSON = new JSONObject(returnJSONString);
-            longLat = NominatimJSON.toCoordinates(queryJSON);
 
-            System.out.println("ReturnJSONString:");
-            System.out.println(returnJSONString);
+
+            // try with all
+            {
+                System.out.println("Elements: ");
+                System.out.println(elements);
+                String https_url = NomatimURL.build(elements);
+                String returnJSONString = NominatimHelper.queryInterface(https_url);
+                JSONObject queryJSON = new JSONObject(returnJSONString);
+                longLat = NominatimJSON.toCoordinates(queryJSON);
+                System.out.println("ReturnJSONString:");
+                System.out.println(returnJSONString);
+            }
+
+            // try with last 3 elements
+            if (Objects.isNull(longLat)) {
+
+                List<String> elements3 = new ArrayList<String>();
+                for (int i = 0; i < elements.size(); ++i) {
+                    if (i >= elements.size() - 3) {
+                        elements3.add(elements.get(i));
+                    }
+                }
+
+
+                System.out.println("Elements3: ");
+                System.out.println(elements3);                
+                String https_url = NomatimURL.build(elements3);
+                String returnJSONString = NominatimHelper.queryInterface(https_url);
+                JSONObject queryJSON = new JSONObject(returnJSONString);
+                longLat = NominatimJSON.toCoordinates(queryJSON);
+                System.out.println("ReturnJSONString:");
+                System.out.println(returnJSONString);
+            }
 
         }
 
         System.out.println("LongLat:");
         System.out.println(longLat);
-        
+
         Thread.sleep(1000);
 
         System.out.println("----");
