f5a3e110821046d05a96dda9aefd18c7a580919f
JoelGaechter <joelgaechter@bluewin.ch>
2020-05-06 18:10:59 +0200

    ScenarioBasicNetworkPreparerTest refactor

--- src/test/java/ch/ethz/idsc/amodtaxi/scenario/ScenarioBasicNetworkPreparerTest.java
+++ src/test/java/ch/ethz/idsc/amodtaxi/scenario/ScenarioBasicNetworkPreparerTest.java
@@ -2,36 +2,48 @@
 package ch.ethz.idsc.amodtaxi.scenario;
 
 import java.io.File;
+import java.util.Arrays;
 
+import ch.ethz.idsc.amodeus.matsim.NetworkLoader;
+import ch.ethz.idsc.amodeus.util.io.CopyFiles;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.google.common.io.Files;
 
 import ch.ethz.idsc.amodeus.util.io.Locate;
 import ch.ethz.idsc.tensor.io.DeleteDirectory;
+import org.matsim.api.core.v01.network.Network;
 
 public class ScenarioBasicNetworkPreparerTest {
+    private static final File WORKING_DIR = new File(Locate.repoFolder(ScenarioBasicNetworkPreparer.class, "amodtaxi"), "test-scenario");
+    private static final File RESOURCE_DIR = new File(Locate.repoFolder(Pt2MatsimXML.class, "amodtaxi"), "resources/test/miniScenario");
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        WORKING_DIR.mkdir();
+
+        CopyFiles.now(RESOURCE_DIR.getAbsolutePath(), WORKING_DIR.getAbsolutePath(), //
+                Arrays.asList(ScenarioLabels.pt2MatSettings, "networkPt2Matsim.xml.gz"), true);
+        /** change pt2Matsim settings to local file system */
+        Pt2MatsimXML.toLocalFileSystem(new File(WORKING_DIR, ScenarioLabels.pt2MatSettings), WORKING_DIR.getAbsolutePath());
+    }
 
     @Test
-    public void test() throws Exception {
-        /* Init */
-        File workingDir = new File(Locate.repoFolder(Pt2MatsimXML.class, "amodtaxi"), "test");
-        File resourcesDir = new File(Locate.repoFolder(Pt2MatsimXML.class, "amodtaxi"), "resources/testScenario");
-        Assert.assertTrue(workingDir.exists() || workingDir.mkdir());
-        ScenarioSetup.in(workingDir, resourcesDir);
-        Files.copy(new File(resourcesDir, "networkPt2Matsim.xml.gz"), new File(workingDir, "networkPt2Matsim.xml.gz"));
-
+    public void test() {
         /* Run function of interest */
-        ScenarioBasicNetworkPreparer.run(workingDir);
+        ScenarioBasicNetworkPreparer.run(WORKING_DIR);
 
         /* Check functionality */
-        Assert.assertTrue(new File(workingDir, ScenarioLabels.network).exists());
-        Assert.assertTrue(new File(workingDir, ScenarioLabels.networkGz).exists());
+        Assert.assertTrue(new File(WORKING_DIR, ScenarioLabels.network).exists());
+        Assert.assertTrue(new File(WORKING_DIR, ScenarioLabels.networkGz).exists());
 
-        /* Clean up */
-        Assert.assertTrue(workingDir.exists());
-        DeleteDirectory.of(workingDir, 2, 14);
-        Assert.assertFalse(workingDir.exists());
+        Network network = NetworkLoader.fromNetworkFile(new File(WORKING_DIR, ScenarioLabels.networkGz));
+        Assert.assertFalse(network.getLinks().isEmpty());
+    }
+
+    @AfterClass
+    public static void cleanUp() throws Exception {
+        DeleteDirectory.of(WORKING_DIR, 2, 5);
     }
 }
