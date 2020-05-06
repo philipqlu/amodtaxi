9f761f68403976582573336c73207a25e29f6083
JoelGaechter <joelgaechter@bluewin.ch>
2020-05-06 18:33:17 +0200

    NetworkCutterUtilTest refactor

--- src/test/java/ch/ethz/idsc/amodtaxi/scenario/NetworkCutterUtilTest.java
+++ src/test/java/ch/ethz/idsc/amodtaxi/scenario/NetworkCutterUtilTest.java
@@ -2,12 +2,14 @@
 package ch.ethz.idsc.amodtaxi.scenario;
 
 import java.io.File;
+import java.util.Collections;
 
+import ch.ethz.idsc.amodeus.util.io.CopyFiles;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.matsim.api.core.v01.network.Network;
-
-import com.google.common.io.Files;
 
 import ch.ethz.idsc.amodeus.matsim.NetworkLoader;
 import ch.ethz.idsc.amodeus.util.io.Locate;
@@ -15,34 +17,33 @@ import ch.ethz.idsc.amodeus.util.network.LinkModes;
 import ch.ethz.idsc.tensor.io.DeleteDirectory;
 
 public class NetworkCutterUtilTest {
+    private static final File WORKING_DIR = new File(Locate.repoFolder(NetworkCutterUtils.class, "amodtaxi"), "test-scenario");
+    private static final File RESOURCE_DIR = new File(Locate.repoFolder(NetworkCutterUtils.class, "amodtaxi"), "resources/test/miniScenario");
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        WORKING_DIR.mkdir();
+        CopyFiles.now(RESOURCE_DIR.getAbsolutePath(), WORKING_DIR.getAbsolutePath(), //
+                Collections.singletonList("networkPt2Matsim.xml.gz"), true);
+    }
 
     @Test
-    public void test() throws Exception {
-        /* Init */
-        File workingDir = new File(Locate.repoFolder(Pt2MatsimXML.class, "amodtaxi"), "test");
-        File resourcesDir = new File(Locate.repoFolder(Pt2MatsimXML.class, "amodtaxi"), "resources/testScenario");
-        Assert.assertTrue(workingDir.exists() || workingDir.mkdir());
-        ScenarioSetup.in(workingDir, resourcesDir);
-
-        /* Using test open street map data to create scenario */
-        System.out.println("Using map.osm from testScenario");
-        File osmFile = new File(workingDir, ScenarioLabels.osmData);
-        Files.copy(new File(resourcesDir, ScenarioLabels.osmData), osmFile);
-
-        /* Load the pt2matsim network */
-        Files.copy(new File(resourcesDir, "networkPt2Matsim.xml.gz"), new File(workingDir, "networkPt2Matsim.xml.gz"));
-        Network networkpt2Matsim = NetworkLoader.fromNetworkFile(new File(workingDir, "networkPt2Matsim.xml.gz"));
+    public void test() {
+        Network networkpt2Matsim = NetworkLoader.fromNetworkFile(new File(WORKING_DIR, "networkPt2Matsim.xml.gz"));
+        int allLinks = networkpt2Matsim.getLinks().size();
 
         /* Run function of interest */
         LinkModes linkModes = new LinkModes("car");
         Network filteredNetwork = NetworkCutterUtils.modeFilter(networkpt2Matsim, linkModes);
+        int filteredLinks = filteredNetwork.getLinks().size();
 
         /* Check functionality */
         Assert.assertTrue(filteredNetwork.getLinks().values().stream().allMatch(link -> link.getAllowedModes().contains("car")));
+        Assert.assertTrue(allLinks > filteredLinks);
+    }
 
-        /* Clean up */
-        Assert.assertTrue(workingDir.exists());
-        DeleteDirectory.of(workingDir, 2, 14);
-        Assert.assertFalse(workingDir.exists());
+    @AfterClass
+    public static void cleanUp() throws Exception {
+        DeleteDirectory.of(WORKING_DIR, 2, 2);
     }
 }
